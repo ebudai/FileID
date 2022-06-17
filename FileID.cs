9c917dccd6e9748b7d2ce7b488f4af590c7dc014
Eric Budai <eric_budai@hotmail.com>
2022-06-17 18:17:01 -0400

    Add project files.

---
+++ FileID.cs
@@ -0,0 +1,17 @@
+﻿#if Windows || NET461
+using OS = Budaisoft.FileSystem.FileIDWindows;
+#else
+using OS = Budaisoft.FileSystem.FileIDPosix;
+#endif
+
+using System.IO;
+
+namespace Budaisoft.FileSystem
+{
+    public static class FileID
+    {
+        public static ulong GetUniqueFileID(string path) => OS.GetUniqueFileID(path);
+        public static ulong GetUniqueFileID(FileInfo file) => GetUniqueFileID(file.FullName);
+        public static ulong GetUniqueFileID(DirectoryInfo folder) => GetUniqueFileID(folder.FullName);
+    }
+}
---
+++ FileIDPosix.cs
@@ -0,0 +1,15 @@
+﻿#if !Windows && !NET461
+using Mono.Unix;
+
+namespace Budaisoft.FileSystem
+{
+    internal static class FileIDPosix
+    {
+        public static ulong GetUniqueFileID(string path)
+        {
+            var file = new UnixFileInfo(path);
+            return (ulong)file.Inode;
+        }
+    }
+}
+#endif
