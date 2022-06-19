cace438dcca4ece6f730d2329433ffc4da3f5ada
Eric Budai <eric_budai@hotmail.com>
2022-06-19 13:11:35 -0400

    removed platform specific packages

--- FileID.cs
+++ FileID.cs
@@ -1,16 +1,15 @@
-﻿#if Windows
-using OS = Budaisoft.FileSystem.FileIDWindows;
-#else
-using OS = Budaisoft.FileSystem.FileIDPosix;
-#endif
-
+﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Budaisoft.FileSystem
 {
     public static class FileID
     {
-        public static ulong GetUniqueFileID(string path) => OS.GetUniqueFileID(path);
+        public static ulong GetUniqueFileID(string path) => RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+            ? FileIDWindows.GetUniqueFileID(path)
+            : FileIDPosix.GetUniqueFileID(path);        
+
         public static ulong GetUniqueFileID(FileSystemInfo file) => GetUniqueFileID(file.FullName);
     }
 }
